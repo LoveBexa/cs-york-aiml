ce5497fb7625b1101b87d3ac48fb40d5156907eb
Liu <bliu1@gblobimppn3md6m.home>
2022-05-15 08:32:23 +0100

    Updated goal test state that actually works by incremental counter

--- breadthfirstsearch-vacuum.py
+++ breadthfirstsearch-vacuum.py
@@ -12,31 +12,44 @@ class State:
         self.actionSuck = actionSuck
 
 
-state_one = State(1, True, True, "Left", 1, 2, 3)
-state_two = State(2, True, True, "Right", 1, 2, 6)
-state_three = State(3, False, True, "Left", 3, 4, 3)
-state_four = State(4, False, True, "Right", 3, 4, 8)
-state_five = State(5, True, False, "Left", 5, 6, 7)
-state_six = State(6, True, False, "Right", 5, 6, 6)
+# state_one = State(1, True, True, "Left", 1, 2, 3)
+# state_two = State(2, True, True, "Right", 1, 2, 6)
+# state_three = State(3, False, True, "Left", 3, 4, 3)
+# state_four = State(4, False, True, "Right", 3, 4, 8)
+# state_five = State(5, True, False, "Left", 5, 6, 7)
+# state_six = State(6, True, False, "Right", 5, 6, 6)
+# state_seven = State(7, False, False, "Left", 7, 8, 7)
+# state_eight = State(8, False, True, "Right", 7, 8, 8)
+
+
+state_one = State(1, False, False, "Left", 1, 2, 3)
+state_two = State(2, False, False, "Right", 1, 2, 6)
+state_three = State(3, False, False, "Left", 3, 4, 3)
+state_four = State(4, False, False, "Right", 3, 4, 8)
+state_five = State(5, False, False, "Left", 5, 6, 7)
+state_six = State(6, False, False, "Right", 5, 6, 6)
 state_seven = State(7, False, False, "Left", 7, 8, 7)
-state_eight = State(8, False, True, "Right", 7, 8, 8)
+state_eight = State(8, False, False, "Right", 7, 8, 8)
+
+
 
 state_list = [state_one, state_two, state_three, state_four, state_five, state_six, state_seven, state_eight]
 
 # The goal state is when the floor is clean and there is no dirt left
 # This checks if all the dirt has been vacuumed 
 def goalTest():
-    counter = 0
+    counter = 1
     for state in state_list:
+        # If either side of floor is dirty then entire floor needs cleaning
         if (state.dirtLeft == True) or (state.dirtRight == True):
             print("The vacuum has not finished cleaning!")
-            return False
-        elif (state.dirtLeft == False) or (state.dirtRight == False):
+            return 0
+        # Checks if floor is clean for each state
+        elif (state.dirtLeft == False) and (state.dirtRight == False):
             counter += 1
-        else:
-            return False
-    if (counter == 7):
-        return False
+            # When each state is completely clean then entire floor is clean!
+            if (counter == 8):
+                return counter
 
 
 frontier = []
@@ -48,10 +61,12 @@ explored = []
 
 first_state = state_two 
 
-# If goal_test = all states have false in dirtLeft and dirtRIght = GOAL ACHIEVED!
+# Checks to see if goal state is met when all states are clean of dirt
+if (goalTest() == 8):
+    sys.exit("Goal achieved - floor is clean! Search ends")
+else:
+    print("The vacuum has not 0 finished cleaning!")
 
-if (goalTest() == False):
-    sys.exit("Goal achieved - floor is clean!")
 
 # frontier -> contains first state
 # explored -> empty 
